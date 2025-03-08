2c877f8a1e4a2101dfefea0a41c9957433dabb52
nminh123 <160375525+nminh123@users.noreply.github.com>
2025-03-08 23:47:04 +0700

    Đã hoàn thành web đầu tiên làm bằng react js

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,27 @@ import { TabButton } from "./components/TabButton.jsx";
 import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedButton);
+  }
+
+  let tabContent = <p>Please click a content</p>
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].desc}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -48,7 +64,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               desc={CORE_CONCEPTS[0].desc}
               image={CORE_CONCEPTS[0].image}
@@ -61,26 +77,21 @@ function App() {
             />
             <CoreConcept
               {...CORE_CONCEPTS[3]}
-            />
+            /> */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem}/>
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].desc}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
--- src/data.js
+++ src/data.js
@@ -34,31 +34,36 @@ export const EXAMPLES = {
     components: {
         title: 'Components',
         desc: 'The core UI building block - compose the user interface by combining multiple components.',
-        code: `function Welcome() {
-            return <h1>Hello, World!!!</h1>;
-        }`
+        code: 
+    `function Welcome() {
+        return <h1>Hello, World!!!</h1>;
+    }`
     },
     jsx: {
         title: 'JSX',
         desc: 'JSX is a syntax extension to JavaScript...',
-        code: `<div>
-            <h1>Welcome {userName}</h1>
-            <p>Time to learn React!</p>
-        </div>`
+        code: `
+    <div>
+        <h1>Welcome {userName}</h1>
+        <p>Time to learn React!</p>
+    </div>`
     },
     props: {
         title: 'Props',
         desc: 'Components accept arbitrary inputs called props...',
-        code: `function Welcome(props) {
-            return <h1>Hello {props.name}</h1>;
-        }`
+        code: 
+    `function Welcome(props) {
+        return <h1>Hello {props.name}</h1>;
+    }`
     },
     state: {
         title: 'State',
         desc: 'State allows React components to change their output over time...',
-        code: `function Counter() {
-            const [count, setCount] = useState(0);
-            return <button onClick={() => setCount(count + 1)}>Click Me {count}</button>;
-        }`
+        code: 
+    `function Counter() {
+        const [count, setCount] = useState(0);
+        return <button onClick={() => setCount(count + 1)}>Click Me {count}
+        </button>;
+    }`
     }
 };
